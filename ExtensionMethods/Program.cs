ac79c3f6e6b6095179bbd4adaaf5061aca9d75a1
aillarionov <Afanasiy.Illarionov@dataart.com>
2020-01-29 15:01:23 +0300

    Add project files.

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string post =
+                "Very very Very very Very very Very very Very very Very very Very very Very very Very very Very very Very very Very very long text here...";
+            var shortenedPost = post.Shorten(5);
+            Console.WriteLine(shortenedPost + "...");
+        }
+    }
+}
---
+++ ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace ExtensionMethods
+{
+    public static class StringExtensions
+    {
+        public static string Shorten(this string str, int numberOfWords)
+        {
+            if (numberOfWords <= 0)
+                throw new ArgumentOutOfRangeException("numberOfWords of words shoudl be greater tnan 0");
+      
+            var words = str.Split(' ');
+
+            if (words.Length <= numberOfWords)
+                return str;
+
+            return string.Join(" ", words.Take(numberOfWords));
+        }
+    }
+}
